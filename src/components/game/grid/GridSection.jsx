9c898a0fad75ac31f7034ef597ebe3ea4006292e
lyongaming59 <owlsmouth@gmail.com>
2022-03-17 05:19:26 -0400

    Game layout ready

--- src/components/game/grid/GridSection.jsx
+++ src/components/game/grid/GridSection.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const GridSection = () => {
     return (
         <td>
-            <button class="button_sel button_select" type="button"></button>
+            <button className="button-sel button-select" type="button"></button>
         </td>
     )
 }
