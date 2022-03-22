5d3c7165ae3c14d96897bd89df2652204bbeb8c9
lyongaming59 <owlsmouth@gmail.com>
2022-03-22 01:49:06 -0400

    Modal ready

--- src/components/player-announcer/PlayerAnnouncer.jsx
+++ src/components/player-announcer/PlayerAnnouncer.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import "./PlayerAnnouncer.css";
+import "../buttons/Buttons.css";
+
 export const PlayerAnnouncer = () => {
   return (
-    <div>PlayerAnnouncer</div>
+    <div className="modal-container">
+      <div className="modal modal-close">
+        <img src="./images/coin.gif" alt="Start" />
+        <div className="modal-text">
+          <h1 id="playerInit">Start Player 1</h1>
+        </div>
+        <button className="button_start start_button" type="button">Begin</button>
+      </div>
+    </div>
   )
 }
